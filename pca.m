213ad08e3b6bbdeeafa642f35ea2a137868db9e4
Edgar Hirama <eh@ubby.io>
2018-12-04 19:54:44 -0200

    initial commit

---
+++ pca.m
@@ -0,0 +1,35 @@
+% @head: MAI5002 - Fundamentos de Matemática Aplicada
+% @auth: André Perez
+% @mail: andre.marcos.perez@usp.br
+% -------------------------------------------------------------------------
+
+%% PCA
+% -------------------------------------------------------------------------
+
+clear;
+clc;
+
+%% Read data
+
+FILENAME = '..\data\facebook.csv';
+facebook = import(FILENAME)';
+
+nrow = size(facebook,1);
+ncol = size(facebook,2);
+
+%% Center data
+
+for i=1:ncol
+   
+    facebook(:,i) = facebook(:,i) / mean(facebook(:,i));
+    
+end
+
+%% Normalize data
+
+%% Clear
+
+clear FILENAME
+clear i
+clear nrow
+clear ncol
